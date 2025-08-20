5cc28d82bdd55346c46b3705d12acae2ecba001b
Abigail Carreto <abycuteflorecita@gmail.com>
2025-08-20 14:02:57 -0600

    Agregar Ejercicio 12.py a 011 al 020

---
+++ 100Problemas/011 al 020/Ejercicio 12.py
@@ -0,0 +1,9 @@
+#Ej.Saludo con año de nacimiento
+
+#Ingresa datos y responde 
+nombre = str(input("Por favor, ingresa tu nombre "))
+print("Holii! un gusto conocerte", nombre)
+
+edad = int(input("Ingrese su edad y adivinare su año de nacimiento "))
+año = 2025-edad
+print("Creo que naciste en el año:", año)
