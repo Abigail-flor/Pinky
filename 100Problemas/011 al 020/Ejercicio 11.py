efd86a3d1098c98f977297c3d6cfccefae1b149b
Abigail Carreto <abycuteflorecita@gmail.com>
2025-08-20 13:04:56 -0600

    Agregar archivo Ejercicio 11.py en 011 al 020

---
+++ 100Problemas/011 al 020/Ejercicio 11.py
@@ -0,0 +1,8 @@
+#Ej.Potencia de un número
+
+#Ingresar el número
+numero = int(input("Escribe un número que desees elevar al cuadrado"))
+
+#Muestra el cuadrado
+print ("El número que escribiste es:", numero)
+print ("Su cuadrado es:", numero**2)
