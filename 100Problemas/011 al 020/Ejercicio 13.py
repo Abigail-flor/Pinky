ac4762067ca9ead2dcfede77e653111209abddc2
Abigail Carreto <abycuteflorecita@gmail.com>
2025-08-21 21:04:07 -0600

    Archivo Ejercicio 12.py

---
+++ 100Problemas/011 al 020/Ejercicio 13.py
@@ -0,0 +1,11 @@
+#Ej.Saludo a múltiples personas
+
+#Ingresar nombres y dar un saludo personalizado
+nombre1 = str(input("Por favor, ingresa tu nombre "))
+print(f"Hola {nombre1}!, un gusto conocerte")
+
+nombre2 = str(input("Por favor, ingrese tu nombre "))
+print(f"Mucho gusto {nombre2}!, espero se encuentre bien")
+
+nombre3 = str(input("Por favor, ingresa tu nombre "))
+print(f"Que hubo {nombre3}!")
--- Ejercicio 0.py
+++ Ejercicio 0.py
@@ -1,1 +1,4 @@
-print("Hola  mundo")
+nombre = str(input("Por favor ingresa tu nombre "))
+edad = int(input("Por favor ingresa tu edad "))
+año = 2025-edad
+print(f"Mucho gusto {nombre}, tu año de nacimiento creo que es: {año}")
